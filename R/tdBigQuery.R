8631c41f61ee69102c530e9b1a49bdf5881d1065
jeffwong <jeff.ct.wong@gmail.com>
2014-02-19 13:43:39 -0800

    fix tryCatch

--- R/tdBigQuery.R
+++ R/tdBigQuery.R
@@ -2,13 +2,13 @@ tdBigQuery = function(q, fetchSize = 100000, verbose = T,...) {
   if(class(tdConnection) == "JDBCConnection") {
     send = dbSendQuery(tdConnection, q, ...)
     data = list()
-    try({while (1) {
+    tryCatch({while (1) {
       moredata = fetch(send, fetchSize)
       data.n = nrow(moredata)
       data = rbind(data, moredata)
       if (verbose) print(sprintf("%s rows fetched", data.n))
       if (data.n < fetchSize) break
-    }}, error = function(e) { })
+    }}, error = function(e) { e })
     dbClearResult(send)
     return (data)
   }
