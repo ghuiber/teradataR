dc449fa3e7f7d81d983cb74a16be32f37973e9e8
martinbel <belmartin@outlook.com>
2014-04-01 13:46:12 -0300

    Create fun_export.R
    Apply a function FUN to a query and export it to a file. 
    If dealing with large datasets, it´s sometimes easier to query the database in chunks, process each chunk and export each chunk to a file. I needed to do this and export the data to another database and couldn´t read the hole table in memmory

---
+++ R/fun_export.R
@@ -0,0 +1,21 @@
+fun_export = function(q, fetchSize = 100000, verbose = T, file = "export.txt", FUN = NULL, ...) {
+  if(class(tdConnection) == "JDBCConnection") {
+    send = dbSendQuery(tdConnection, q, ...)
+    data = list()
+    top = fetch(send, 1)
+    top = FUN(top)
+    write.table(top, file = file, quote = FALSE, append = FALSE, sep = ";",
+                dec = ",", row.names = FALSE, col.names = FALSE)
+    tryCatch({while (1) {
+      moredata = fetch(send, fetchSize)
+      data.n = nrow(moredata)
+      data = FUN(moredata)
+      write.table(data, file = file, quote = FALSE, append = TRUE, sep = ";",
+                  dec = ",", row.names = FALSE, col.names = FALSE)      
+      if (verbose) print(sprintf("%s rows fetched", data.n))
+      if (data.n < fetchSize) break
+    }}, error = function(e) { e })
+    dbClearResult(send)
+    return (data)
+    }
+}
